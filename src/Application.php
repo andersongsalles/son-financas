581ee83dd8478394087fa7a4e028ff944f16ac0a
Anderson Home <agsalles@gmail.com>
2017-04-28 20:12:22 -0300

    Primeira Rota

---
+++ src/Application.php
@@ -0,0 +1,52 @@
+<?php
+declare(strict_types=1);
+namespace SONFin;
+
+
+use SONFin\Plugins\PluginInterface;
+
+class Application
+{
+    private $_serviceContainer;
+
+    /**
+     * Application constructor.
+     * @param ServiceContainerInterface $serviceContainer
+     */
+    public function __construct(ServiceContainerInterface $serviceContainer)
+    {
+        $this->_serviceContainer = $serviceContainer;
+    }
+
+    public function service($name){
+        return $this->_serviceContainer->get($name);
+    }
+
+    public function addService(string $name, $service): void
+    {
+        if(is_callable($service)){
+            $this->_serviceContainer->addLazy($name, $service);
+        }else{
+            $this->_serviceContainer->add($name, $service);
+        }
+    }
+
+    public function plugin(PluginInterface $plugin): void
+    {
+        $plugin->register($this->_serviceContainer);
+    }
+
+    public function get($path, $action, $name = null): Application
+    {
+        $routing  = $this->service('routing');
+        $routing->get($name, $path, $action);
+        return $this;
+    }
+
+    public function start()
+    {
+        $route = $this->service('route');
+        $callable = $route->handler;
+        $callable();
+    }
+}
---
+++ src/ServiceContainerInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace SONFin;
+
+
+interface ServiceContainerInterface
+{
+    public function add(string $name, $service);
+    public function addLazy(string $name, callable $callable);
+    public function get(string $name);
+    public function has(string $name);
+}
